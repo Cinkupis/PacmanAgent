43513693119ac7eb3becf98d8947a67ea8b29e5e
Cinkupis <d045312@gmail.com>
2018-11-28 02:06:26 +0000

    Additional small changes quality of life

--- mdpAgents.py
+++ mdpAgents.py
@@ -13,8 +13,8 @@ REWARD_GHOST = -2
 PROBABILITY_FORWARDS = api.directionProb
 PROBABILITY_SIDEWAYS = (1 - api.directionProb) / 2
 
-CONVERGENCE_THRESHOLD = 0.1
-GAMMA = 0.9
+CONVERGENCE_THRESHOLD = 0.01
+GAMMA = 0.95
 
 class MDPAgent(Agent):
 
@@ -26,10 +26,10 @@ class MDPAgent(Agent):
         self.position = None
         self.walls = None
         self.legalDirections = None
-        self.knownGhosts = None
-        self.knownGhostsWithTimer = None
-        self.knownFood = None
-        self.knownCapsules = None
+        self.ghostList = None
+        self.ghostWithTimerList = None
+        self.foodList = None
+        self.capsuleList = None
         self.offsetDirectionalMapping = None
         self.oppositeOf = None
         self.previousMap = []
@@ -38,40 +38,36 @@ class MDPAgent(Agent):
         self.mapHeight = 0
         self.mapWidth = 0
 
-    def initializeMap(self):
-        for y in range(self.mapHeight):
-            for x in range(self.mapWidth):
-                spot = (x, y)
-                if self.notWall(spot):
-                    self.previousMap[x][y] = 0
-        self.currentMap = self.previousMap
-
-    def notWall(self, spot):
-        return spot not in self.walls
+    def notWall(self, square):
+        return square not in self.walls
 
     def addTuples(self, thisTuple, direction):
         return tuple(map(sum, zip(thisTuple, self.offsetDirectionalMapping[direction])))
 
-    def reward(self, spot):
-        distance = 1000
-        indexOfClosestGhost = None
-        rewards = REWARD_STANDARD
-        for ghost in self.knownGhosts:
-            if distance > self.getDiagonalDist(spot, ghost):
-                distance = self.getDiagonalDist(spot, ghost)
-                indexOfClosestGhost = self.knownGhosts.index(ghost)
-                if distance == 0:
-                    distance = 1
+    def rewardOfState(self, square):
+        squareToGhostDistance = sys.maxint
+        pointsAtState = REWARD_STANDARD
 
-        if self.knownGhostsWithTimer[indexOfClosestGhost][1] < 5 and distance < 5:
-            rewards += REWARD_GHOST / distance
-        if spot in self.knownGhosts:
-            rewards += REWARD_GHOST
-        if spot in self.knownCapsules:
-            rewards += REWARD_CAPSULE
-        if spot in self.knownFood:
-            rewards += REWARD_FOOD
-        return rewards
+        for ghost in self.ghostList:
+            if squareToGhostDistance > self.getDiagonalDist(square, ghost):
+                squareToGhostDistance = self.getDiagonalDist(square, ghost)
+                indexOfClosestGhost = self.ghostList.index(ghost)
+                if squareToGhostDistance == 0:
+                    squareToGhostDistance = 1
+
+        if self.ghostWithTimerList[indexOfClosestGhost][1] < 5 and squareToGhostDistance < 5:
+            pointsAtState += REWARD_GHOST / squareToGhostDistance
+
+        if square in self.ghostList:
+            pointsAtState += REWARD_GHOST
+
+        if square in self.capsuleList:
+            pointsAtState += REWARD_CAPSULE
+
+        if square in self.foodList:
+            pointsAtState += REWARD_FOOD
+
+        return pointsAtState
 
     def getDiagonalDist(self, objectA, objectB):
         """
@@ -119,7 +115,7 @@ class MDPAgent(Agent):
             utility += self.calculateUtilitySideways(currentSpot, Directions.RIGHT[direction])
             if utility > maxUtility:
                 maxUtility = utility
-        self.currentMap[x][y] = round(GAMMA * maxUtility + self.reward(currentSpot), 3)
+        self.currentMap[x][y] = round(GAMMA * maxUtility + self.rewardOfState(currentSpot), 3)
 
     def iterateOnce(self):
         for y in range(1, self.mapHeight - 1):
@@ -127,9 +123,9 @@ class MDPAgent(Agent):
                 currentSpot = (x, y)
                 if self.notWall(currentSpot):
                     self.calculateUtility(currentSpot)
+                    self.sumOfAllUtilities += self.currentMap[x][y]
                 else:
                     self.currentMap[x][y] = self.previousMap[x][y]
-                self.sumOfAllUtilities += self.currentMap[x][y]
 
     def runIterationsUntilConverge(self):
         while True:
@@ -140,7 +136,7 @@ class MDPAgent(Agent):
             if abs(abs(previousSumOfAllUtilities) - abs(self.sumOfAllUtilities)) <= CONVERGENCE_THRESHOLD:
                 break
 
-    def maximumExpectedUtility(self):
+    def directionWithMaximumExpectedUtility(self):
         utilities = {}
         for direction in self.legalDirections:
             nextSpot = self.addTuples(self.position, direction)
@@ -161,10 +157,23 @@ class MDPAgent(Agent):
         if Directions.STOP in self.legalDirections:
             self.legalDirections.remove(Directions.STOP)
 
-        self.knownGhosts = api.ghosts(state)
-        self.knownGhostsWithTimer = api.ghostStatesWithTimes(state)
-        self.knownFood = api.food(state)
-        self.knownCapsules = api.capsules(state)
+        self.ghostList = api.ghosts(state)
+        self.ghostWithTimerList = api.ghostStatesWithTimes(state)
+        self.foodList = api.food(state)
+        self.capsuleList = api.capsules(state)
+
+        self.initializeMap()
+
+    def initializeMap(self):
+        # Python doesn't have a simple initialization of 2D arrays like, for example, in Java 'int var = new int[][];'
+        # So we have to resolve to fancy one liners to do the trick.
+        # In this case we initialize every single square of the map to -1000. This value indicates there is a wall.
+        # Later on, we adjust the values for squares that aren't exactly walls.
+        self.previousMap = [[0 if self.notWall((x, y))
+                            else -10
+                                for y in range(self.mapHeight)]
+                                    for x in range(self.mapWidth)]
+        self.currentMap = self.previousMap
 
     def instantiateOnce(self, state):
         """
@@ -178,36 +187,20 @@ class MDPAgent(Agent):
             Directions.EAST: (1, 0),
             Directions.SOUTH: (0, -1)
         }
-        self.oppositeOf = {
-            Directions.WEST: Directions.EAST,
-            Directions.NORTH: Directions.SOUTH,
-            Directions.EAST: Directions.WEST,
-            Directions.SOUTH: Directions.NORTH
-        }
-
         self.mapWidth = self.walls[-1][0] + 1
         self.mapHeight = self.walls[-1][1] + 1
-        # Python doesn't have a simple initialization of 2D arrays like, for example, in Java 'int var = new int[][];'
-        # So we have to resolve to fancy one liners to do the trick.
-        # In this case we initialize every single square of the map to -1000. This value indicates there is a wall.
-        # Later on, we adjust the values for squares that aren't exactly walls.
-        self.previousMap = [[-1000 if (x, y) in self.walls
-                             else 0
-                             for y in range(self.mapHeight)]
-                            for x in range(self.mapWidth)]
-        self.currentMap = self.previousMap
         self.doOnce = False
 
     def getAction(self, state):
-        # updates information about its' environment
-        self.renewInformation(state)
-        # Once per game
+        # Once per game, some static info
         if self.doOnce:
             self.instantiateOnce(state)
 
-        self.initializeMap()
+        # updates information about its' environment
+        self.renewInformation(state)
+
         self.runIterationsUntilConverge()
-        bestDirection = self.maximumExpectedUtility()
+        bestDirection = self.directionWithMaximumExpectedUtility()
 
         return api.makeMove(bestDirection, self.legalDirections)
 
@@ -220,12 +213,14 @@ class MDPAgent(Agent):
         self.position = None
         self.walls = None
         self.legalDirections = None
-        self.knownGhosts = None
-        self.knownGhostsWithTimer = None
-        self.knownFood = None
-        self.knownCapsules = None
+        self.ghostList = None
+        self.ghostWithTimerList = None
+        self.foodList = None
+        self.capsuleList = None
         self.offsetDirectionalMapping = None
         self.oppositeOf = None
         self.previousMap = []
         self.currentMap = []
         self.sumOfAllUtilities = 0
+        self.mapHeight = 0
+        self.mapWidth = 0
